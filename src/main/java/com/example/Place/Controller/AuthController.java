0207d8a88e0eb2f32d4312e93a8f31495044e434
tushar raman <101983118+Raman0tushar@users.noreply.github.com>
2026-07-05 22:09:20 +0530

    Update AuthController.java

--- src/main/java/com/example/Place/Controller/AuthController.java
+++ src/main/java/com/example/Place/Controller/AuthController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://divyanshutiwari02.github.io/Placementor-FrontEnd/")
 public class AuthController {
 
     @Autowired
