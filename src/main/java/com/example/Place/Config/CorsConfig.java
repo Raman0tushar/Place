893871145f4f98942a8d326b70293534b0918c7a
tushar raman <101983118+Raman0tushar@users.noreply.github.com>
2026-07-12 09:00:52 +0530

    Update CORS configuration to allow OPTIONS method

--- src/main/java/com/example/Place/Config/CorsConfig.java
+++ src/main/java/com/example/Place/Config/CorsConfig.java
@@ -1,17 +1,20 @@
 package com.example.Place.Config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api")
-                .allowedOrigins("https://divyanshutiwari02.github.io/","http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://divyanshutiwari02.github.io"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
