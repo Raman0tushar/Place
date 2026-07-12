cca0980c1126e7c2ad60fbac191f91eed0abddc7
tushar raman <101983118+Raman0tushar@users.noreply.github.com>
2026-07-12 09:00:03 +0530

    Enable CORS in SecurityConfig

--- src/main/java/com/example/Place/Config/SecurityConfig.java
+++ src/main/java/com/example/Place/Config/SecurityConfig.java
@@ -1,23 +1,25 @@
 package com.example.Place.Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+
+        http
+                .cors(Customizer.withDefaults())   // Enable CORS
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 );
+
         return http.build();
     }
 }
