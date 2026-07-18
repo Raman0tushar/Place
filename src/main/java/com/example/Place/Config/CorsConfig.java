cb1cba59b277d92b6b2bd8c9223372a053e86aa4
tushar raman <101983118+Raman0tushar@users.noreply.github.com>
2026-07-18 22:24:13 +0530

    Update CorsConfig.java

--- src/main/java/com/example/Place/Config/CorsConfig.java
+++ src/main/java/com/example/Place/Config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://divyanshutiwari02.github.io"
+                        "https://placementor-front-end.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
